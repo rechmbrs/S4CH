092d0d13a4eba02160cd5712de004cabd5265702
Ron Chambers <rechmbrs@gmail.com>
2022-09-26 18:26:28 -0500

    Add files via upload
    Fixes

--- manifest.js
+++ manifest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // manifest.js
 
 const versionTitle =
@@ -41,7 +40,7 @@ var sponsor;
 var sponsorWeb;
 var sponsorLogo
 if (sponsorNum == 1) {
-    sponsor = "Google";
+    sponsor = "";
     sponsorWeb = "https://www.google.com";
     sponsorLogo = "sm-logo-tp1.png";
 } else if (sponsorNum == 2) {
@@ -50,57 +49,4 @@ if (sponsorNum == 1) {
     sponsorLogo = "sm-logo-tp1a.png";
 }
 
-=======
-// manifest.js
-
-const versionTitle =
-    "Scales For Chromatic";
-
-const versionNum =
-    "06.50.20";
-
-const versionDate =
-    "26 Sep 2022";
-
-const versionUrl =
-    versionTitle +
-    versionDate +
-    ".zip";
-
-const author = "Ron Chambers";
-
-const email = "rechmbrs at gmail.com";
-
-const cRight = "&copy;2022 RONC USA";
-
-const header =
-    "(C-tuned)" +
-    "<br>" +
-    "by " +
-    author +
-    "<br>" +
-    "Version: " +
-    versionNum +
-    "<br>" +
-    versionDate +
-    "<br>" +
-    cRight +
-    "<br>" +
-    email;
-
-const sponsorNum = 1;
-var sponsor;
-var sponsorWeb;
-var sponsorLogo
-if (sponsorNum == 1) {
-    sponsor = "Google";
-    sponsorWeb = "https://www.google.com";
-    sponsorLogo = "sm-logo-tp1.png";
-} else if (sponsorNum == 2) {
-    sponsor = "SlideMeister website";
-    sponsorWeb = "https://www.slideMeister.com";
-    sponsorLogo = "sm-logo-tp1a.png";
-}
-
->>>>>>> f598108b3122b93bc2e034d51aef2bbcd7f2d0d7
 // manifest.js
